b8d24c20c07b13efaecd3f8e66450f5810b03c9a
jerry Ifeanyi <jerrycifeanyi@gmail.com>
2020-03-18 12:04:29 -0700

    final prod commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Container} from 'reactstrap'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <div className="row">
+                  <div className="text-center text-title ">
+                      <h2 className="text-blue display-3">WELCOME TO ZDMANAGEMENT SERVICES</h2>
+                      <hr></hr>
+                      <br></br>
+                      <h3> This webpage is still under construction </h3>
+                      <br></br><br></br>
+                      <h6 className="text-yellow">
+                          contact us via email:info@zdmanagementservices.com
+                      </h6>
+
+                      <br></br><br></br><br></br>
+
+                      <h6 className="text-yellow">
+                          By JerryWebman:jerrycifeanyi@gmail.com
+                      </h6>
+                  </div>
+              </div>
+    </Container>
+        
+
+);
 }
 
 export default App;
